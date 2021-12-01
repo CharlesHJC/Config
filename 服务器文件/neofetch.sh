7779f80b3ab0935c4678af5ee982a8f19adbbaa0
何嘉琛 <charles_hjc@outlook.com>
2021-12-01 16:25:13 +0800

    Update neofetch.sh

--- 服务器文件/neofetch.sh
+++ 服务器文件/neofetch.sh
@@ -1,4 +1,13 @@
+###
+ # @Author: CharlesHE
+ # @Date: 2021-12-01 16:23:18
+ # @LastEditTime: 2021-12-01 16:24:59
+ # @LastEditors: CharlesHE
+ # @Description: 
+ # @FilePath: \undefinedc:\Users\E116022\Desktop\neofetch.sh
+### 
+
 #/etc/profile.d/neofetch.sh
 
-#/bin/sh
+!#/bin/bash
 neofetch
